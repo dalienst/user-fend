d383a575ebdfec63b3148067756ba8f5e4e32d28
dalienst <oduordalienst@gmail.com>
2022-12-21 19:18:13 +0300

    fix landing page design

--- src/layouts/Landing/Landing.js
+++ src/layouts/Landing/Landing.js
@@ -7,13 +7,13 @@ const Landing = () => {
         <div className="landing-page">
             <Navbar />
             <div className="hero-section">
+                <div className="hero-right">
+                    <img src={heroImg} alt=""/>
+                </div>
                 <div className="hero-left">
                     <h1 className="hero-title">Easily create, view and edit your profile</h1>
                     <p className="hero-description">Elephant Room is a profile tool that allows you to easily create and edit your profile to one that suits you perfectly.</p>
                     <button className="btn btn-primary">Get Started</button>
-                </div>
-                <div className="hero-right">
-                    <img src={heroImg} alt=""/>
                 </div>
             </div>
         </div>
