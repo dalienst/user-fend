a5fe9a83f299338829d0e8d9cc109d100110fdf1
dalienst <oduordalienst@gmail.com>
2022-12-23 15:12:14 +0300

    fix button

--- src/layouts/Landing/Landing.js
+++ src/layouts/Landing/Landing.js
@@ -15,7 +15,7 @@ const Landing = () => {
                 <div className="hero-left">
                     <h1 className="hero-title">Easily create, view and edit your profile</h1>
                     <p className="hero-description">Elephant Room is a profile tool that allows you to easily create and edit your profile to one that suits you perfectly.</p>
-                    <Link to={publicLinks.Login} className="btn btn-primary">Get Started</Link>
+                    <Link to={publicLinks.Login} className="nav-button">Get Started</Link>
                 </div>
             </div>
         </div>
